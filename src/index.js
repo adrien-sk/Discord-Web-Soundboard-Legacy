04de69728c0b1663492423e25be392476a077f3a
Adrien <skrzypczak.adrien@gmail.com>
2020-06-05 00:01:06 +0200

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<h1>Discord Web Soundboard</h1>
+		);
+	}
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
